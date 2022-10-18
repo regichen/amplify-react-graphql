01e5d8c21c4aaff1143bdac0e1ffb24e7e10e9a4
陈麟鑫 <chenlx@Linxins-MacBook-Pro.local>
2022-10-17 22:24:58 -0400

    “auth”

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactgraphqle658e3e2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
